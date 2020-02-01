91caa6fa5dca24826becbda026438f0ab9fb24eb
rafaelwi <rafwiskailnicki@gmail.com>
2020-01-31 23:29:53 -0500

    Added CL args

---
+++ randf_cliargs.py
@@ -0,0 +1,6 @@
+import argparse
+
+
+def addArgs(p: argparse.ArgumentParser):
+    p.add_argument("-i", "--input", help="name of the input file")
+    p.add_argument("-o", "--output", help="name of the output file, default to the name of the input file with a .pdf extension")
