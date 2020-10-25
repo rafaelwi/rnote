20b0f3953a3aa1076145c9595f1cebaade16824b
rafaelwi <rafwiskailnicki@gmail.com>
2020-10-25 16:57:12 -0400

    Cleaning up app file

--- app.py
+++ app.py
@@ -5,55 +5,42 @@ import uuid
 import imp
 import rnote_for_webapp as rnote
 
+DEFAULT_TEXT = """// Enter your note below and click 'compile' to generate it!
+.pp theme modern
+.pp size letter
+.pp margin normal
+.pp title My RNote Demo Note
+.pp align vert
+
+= Some test text
+"""
+
 # Displays the homepage
 # Page has to go into templates regardless of whether it actually uses the template features or not
-@app.route('/')
-def index(pdf_filename='./out'):
-    print('Filename: {}'.format(pdf_filename))
-    return render_template('index.html', pdf_filename=pdf_filename)
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html', pdf_filename='./out', code=DEFAULT_TEXT)
 
-@app.route('/', methods=['POST'])
-def indexRender():
-    code = request.form['code']
-    filename = 'temp/' + str(uuid.uuid4()) + '.pdf'
-
-    # Send off the code and filename to be processed
-    rnote.run(code, filename)
-    return index(filename)
-
-# Displays the pdf
-# TODO: Accept an id to display the correct file to the correct user
-@app.route('/out')
-def peedeeeff():
-    return send_from_directory('', 'out.pdf')
-
-# Test route
-@app.route('/hello')
-def hello():
-    return 'Hello world!'
-
-# This is how you'd use a template with an input from the url
-# Could be useful for displaying pdf?
-@app.route('/user/<string:username>')
-def user(username):
-    return 'User {}'.format(username)
-
-@app.route('/render', methods=['POST'])
-def render():
     # Get the "code" and generate a new filename with a UUID
     code = request.form['code']
     filename = 'temp/' + str(uuid.uuid4()) + '.pdf'
 
     # Send off the code and filename to be processed
     rnote.run(code, filename)
-    index(filename)
-    #return render_template('index.html', pdf_filename=filename)
-    #return redirect(url_for('index'), pdf_filename=filename)
+    return render_template('index.html', pdf_filename=filename, code=code)
+
+
+# Displays the pdf
+@app.route('/out')
+def peedeeeff():
+    return send_from_directory('', 'out.pdf')
 
 
 @app.route('/temp/<string:filename>')
 def displayNewPdf(filename):
     return send_from_directory('temp', filename)
+
 
 # This is how you do error handling
 # We need to pass in the error parameter in regardless
@@ -62,6 +49,3 @@ def displayNewPdf(filename):
 @app.errorhandler(404)
 def error_404(error):
     return render_template('error.html'), 404
-
-# Consider using user cookies or sessions to store things? Maybe that's too naughty
-
